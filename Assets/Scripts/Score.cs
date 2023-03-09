cf8229afa08996bc8118afac30f5bf6250729c2e
AnnaReid22 <anna.reid@live.com>
2023-03-08 20:38:41 -0800

    big changeset, added intro and final scene and some math scores, some setup for scenes that are not implemented yet

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
-    public static int trash_score = 0; 
+    public static int trash_score = 0;
+	public static int planting_score = 0;
+	public static int pond_score = 0;
+	public static int koifish_score = 0;
+	public static int decorations_score = 0;
+	public TMP_Text result;
+	
+	private float max_total_score = 100.0f;
+	
+	public static float total_score = 0.0f;
+	
+	void Start()
+	{
+		
+	}
+	
+	    // Update is called once per frame
+    void Update()
+    {
+		if ("6_Score_Scene" == SceneManager.GetActiveScene().name)
+		{
+			total_score = (trash_score + planting_score + pond_score + koifish_score + decorations_score) / max_total_score;
+			result.text = "Your Score is " + total_score.ToString();
+			
+		}
+		else
+		{
+			total_score = 0.0f;
+		}
+    }
+	
+
 
 }
