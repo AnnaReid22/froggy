24027bf260c5efb5a4b756e7495eb6a8ca456c1c
AnnaReid22 <anna.reid@live.com>
2023-03-19 13:34:33 -0700

    koifish scoring

--- Assets/Scripts/Froggy.cs
+++ Assets/Scripts/Froggy.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Froggy : MonoBehaviour
 {
+
+	public GameObject pond;
+	
     void Start()
     {
         Scene scene = SceneManager.GetActiveScene(); // get current scene name 
         if (scene.name == "1_Trash_Scene") { 
             // set starting position in the trash scene
             transform.position = new Vector3(0f, -1.46267f, 0); 
+        }
+		if (scene.name == "4_Koifish_Scene") { 
+            // set starting position in the trash scene
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), pond.GetComponent<Collider2D>());
         }   
     }
 
